dd4fb1b14893626b313aadb4dfaf42e139775c29
ariskusnul widitama <ariskusnul@gmail.com>
2022-08-29 14:31:21 +0700

    initial commit

---
+++ src/test/java/com/example/ist/challenge/aris_kusnul_widitama/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ist.challenge.aris_kusnul_widitama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
